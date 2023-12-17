bd8ec59c25e02150804d781682fffa3ff0275e29
bruno <bruno.resende@gmx.com>
2023-12-17 17:22:07 -0300

    First Commit

---
+++ 2022/day-01/src/bin/part-1.rs
@@ -0,0 +1,8 @@
+use day_01::process_part1;
+use std::fs;
+
+fn main() {
+    let input = fs::read_to_string("input.txt").expect("Failed to read input.txt");
+    let result = process_part1(&input);
+    println!("Result: {}", result);
+}
