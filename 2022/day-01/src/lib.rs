1f66876bbdcf06c072fe4876f414ed85af97cd88
bruno <bruno.resende@gmx.com>
2023-12-24 11:08:23 -0300

    Day 1 - Ex. 2 Done

--- 2022/day-01/src/lib.rs
+++ 2022/day-01/src/lib.rs
@@ -7,6 +7,13 @@ pub fn process_part1(input: &str) -> u32 {
     input.split("\n\n").map(get_group_sum).max().unwrap()
 }
 
+pub fn process_part2(input: &str) -> u32 {
+    let mut elves_calores = input.split("\n\n").map(get_group_sum).collect::<Vec<u32>>();
+
+    elves_calores.sort_by(|a, b| b.cmp(a));
+    elves_calores.iter().take(3).sum()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -30,5 +37,11 @@ mod tests {
     fn part_1_works() {
         let result = process_part1(INPUT);
         assert_eq!(result, 24000);
+    }
+
+    #[test]
+    fn part_2_works() {
+        let result = process_part2(INPUT);
+        assert_eq!(result, 45000);
     }
 }
