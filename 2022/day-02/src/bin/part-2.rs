9aaa2c02b15979952f4c05521c7aa0b8b917a7c1
bruno <bruno.resende@gmx.com>
2023-12-27 10:22:53 -0300

    Feat: Day 2 Completed

---
+++ 2022/day-02/src/bin/part-2.rs
@@ -0,0 +1,8 @@
+use day_02::process_part2;
+use std::fs;
+
+fn main() {
+    let input = fs::read_to_string("input.txt").expect("Failed to read input.txt");
+    let result = process_part2(&input);
+    println!("Result: {}", result);
+}
--- 2022/day-02/src/lib.rs
+++ 2022/day-02/src/lib.rs
@@ -1,14 +1,99 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::collections::HashMap;
+/*
+First Challenge
+
+Opponent Plays
+A Rock
+B Paper
+C Scissors
+
+My plays
+X Rock
+Y Paper
+Z Scissors
+*/
+
+pub fn process_part1(input: &str) -> u32 {
+    input.lines().map(calculate_firt_duel_score).sum()
+}
+
+/*
+Second Challenge
+
+Opponent Plays
+A Rock
+B Paper
+C Scissors
+
+Outcome
+X Lose
+Y Draw
+C Win
+*/
+
+pub fn process_part2(input: &str) -> u32 {
+    input.lines().map(calculate_second_duel_score).sum()
+}
+
+fn calculate_firt_duel_score(duel: &str) -> u32 {
+    let mut moves = duel.split_whitespace();
+
+    let opponent_move = moves.next().unwrap();
+    let my_move = moves.next().unwrap();
+
+    let my_move_values = HashMap::from([("X", 3), ("Y", 2), ("Z", 1)]);
+
+    let game_values = HashMap::from([
+        ("A", HashMap::from([("X", 3), ("Y", 6), ("Z", 0)])),
+        ("B", HashMap::from([("X", 0), ("Y", 3), ("Z", 6)])),
+        ("C", HashMap::from([("X", 6), ("Y", 0), ("Z", 3)])),
+    ]);
+
+    let my_move_score = my_move_values[my_move];
+    let game_score = game_values[opponent_move][my_move];
+
+    my_move_score + game_score
+}
+
+fn calculate_second_duel_score(duel: &str) -> u32 {
+    let mut moves = duel.split_whitespace();
+
+    let opponent_move = moves.next().unwrap();
+    let game_outcome = moves.next().unwrap();
+
+    let my_move_values = HashMap::from([("A", 1), ("B", 2), ("C", 3)]);
+    let game_outcome_values = HashMap::from([("X", 0), ("Y", 3), ("Z", 6)]);
+
+    let required_move = HashMap::from([
+        ("A", HashMap::from([("X", "C"), ("Y", "A"), ("Z", "B")])),
+        ("B", HashMap::from([("X", "A"), ("Y", "B"), ("Z", "C")])),
+        ("C", HashMap::from([("X", "B"), ("Y", "C"), ("Z", "A")])),
+    ]);
+
+    let my_move = required_move[opponent_move][game_outcome];
+    let game_score = game_outcome_values[game_outcome];
+    let my_move_score = my_move_values[my_move];
+
+    my_move_score + game_score
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    const INPUT: &str = "A Y
+    B X
+    C Z";
+
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn part_1_works() {
+        let result = process_part1(INPUT);
+        assert_eq!(result, 15);
+    }
+
+    #[test]
+    fn part_2_works() {
+        let result = process_part2(INPUT);
+        assert_eq!(result, 12);
     }
 }
